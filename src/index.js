28c36904825c397869837238272c53c7e636ffad
Misdely Morales <misdely.m@gmail.com>
2022-07-04 10:58:29 -0400

    function isValid

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import validator from './validator.js';
 
+function ejecutarEvaluacion(e){
+    const input = document.querySelector("#creditCardNumber").value;
+    let cardNumber = input.split('').reverse();
+    const validationResult = validator.isValid(cardNumber);
+ }
+
+document.getElementById('butVal').addEventListener('click', ejecutarEvaluacion)
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,8 +1,23 @@
 const validator = {
-  let cardNumber = document.querySelector('#creditCardNumber');
+  
+  isValid: function(cardNumber){
+    let sum = 0
+
+    for(var i=0; i<cardNumber.length; i++){
+
+      let currentNumber= +cardNumber[i];
+      if (i%2 !=0){
+        currentNumber *= 2;
+          if (currentNumber>9){
+            currentNumber= parseInt(String(currentNumber).charAt(0)) + parseInt(String(currentNumber).charAt(1))
+          }
+        }
+      sum += currentNumber;
+    }
+    return sum%10 === 0; 
+     
+  }
+   
 };
 
-
 export default validator;
-
-console.log(array.from(cardNumber));
