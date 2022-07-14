52725a6a0953612e3664ecc8be8549b96632f0b6
Misdely Morales <misdely.m@gmail.com>
2022-07-14 09:56:49 -0400

    comentarios

--- src/validator.js
+++ src/validator.js
@@ -1,14 +1,14 @@
 const validator = {
-  // cardNumber entra como número
+  // CardNumber es un string vacío
   cardNumber: '', 
 
   //Método isValid
   isValid: function(input){
-    if (input !== undefined){
+    if (input !== undefined){ // si la función es diferente de undefined input va a ser igual al valor de cardNumber 
       this.cardNumber = input
     }
 
-    if(!this.cardNumber || this.cardNumber==='') return false;
+    if(!this.cardNumber || this.cardNumber==='') return false; 
 
     let sum = 0
     let reverseNumber = this.cardNumber.split('').reverse();  // Se guarda en el numero invertido en un arreglo
